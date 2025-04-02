13a249cb3d6562369d7896c1ba36da7f5117c9d6
IkerEga <egana.iker@uni.eus>
2025-04-02 16:10:25 +0200

    ruta guztiak ondo eginda

--- index.php
+++ index.php
@@ -66,17 +66,17 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="./index.html">Hasiera</a>
+                            <a class="nav-link active" aria-current="page" href="index.php">Hasiera</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../PRODUKTUAK/Produktuak.html">Produktuak</a>
+                            <a class="nav-link" href="./PRODUKTUAK/Produktuak.php">Produktuak</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../KONTAKTUA/Kontaktua.html">Kontaktua</a>
+                            <a class="nav-link" href="./KONTAKTUA/Kontaktua.php">Kontaktua</a>
                         </li>
                     </ul>
                     <button class="btn">
-                        <a href="./SAIOA_HASI/Saioa_Hasi.html">
+                        <a href="./SAIOA_HASI/Saioa_Hasi.php">
                             <img src="./HASIERA/img/imagen inicio sesion copy.png" class="img-fluid">
                         </a>
                     </button>
