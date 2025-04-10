49f616afe705bf00e456c6a51d966636bb90ec56
AnasBrahim <brahim.anas@uni.eus>
2025-04-10 13:08:42 +0200

    langileak jquery konpontzea

--- LANGILEAK/Langileak.php
+++ LANGILEAK/Langileak.php
@@ -11,6 +11,7 @@ session_start();
     <title>Langileak</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="Langileak.css">
 </head>
 
